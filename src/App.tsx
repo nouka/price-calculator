2966c86980f719f4596c846c42cd8e7d6a9d30eb
ノーカ <t-nouka@oz-vision.co.jp>
2019-07-18 01:14:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {}
+interface State {
+  price: number;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      price: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(price: any) {
+    this.setState({ price });
+  }
+  render() {
+    return (
+      <>
+        商品金額:
+        <input
+          type="text"
+          value={this.state.price}
+          onChange={e => this.handleChange(e.target.value)}
+        />
+        <ul>
+          <li>20%引き：{Math.floor(this.state.price * 0.8 * 1.08)}円</li>
+          <li>30%引き：{Math.floor(this.state.price * 0.7 * 1.08)}円</li>
+          <li>40%引き：{Math.floor(this.state.price * 0.6 * 1.08)}円</li>
+          <li>50%引き：{Math.floor(this.state.price * 0.5 * 1.08)}円</li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
