704d379a461c6e8dfb3a370966b28d5839c18179
ノーカ <t-nouka@oz-vision.co.jp>
2019-07-18 07:38:21 +0900

    fix: 修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,47 @@ import './App.css';
 
 interface Props {}
 interface State {
-  price: number;
+  price: any;
 }
 
 class App extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      price: 0
+      price: ''
     };
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(price: any) {
-    this.setState({ price });
+    if (price.match(/[0-9]+/) || price.length === 0) {
+      this.setState({ price });
+    }
   }
   render() {
+    const tax = 1.08;
     return (
       <>
-        商品金額:
+        <h1>商品金額計算機</h1>
         <input
-          type="text"
+          type="number"
           value={this.state.price}
+          placeholder="ここに商品金額(税抜)を入力してください。"
+          className="price"
           onChange={e => this.handleChange(e.target.value)}
         />
         <ul>
-          <li>20%引き：{Math.floor(this.state.price * 0.8 * 1.08)}円</li>
-          <li>30%引き：{Math.floor(this.state.price * 0.7 * 1.08)}円</li>
-          <li>40%引き：{Math.floor(this.state.price * 0.6 * 1.08)}円</li>
-          <li>50%引き：{Math.floor(this.state.price * 0.5 * 1.08)}円</li>
+          <li>
+            20%引き<span>{Math.floor(this.state.price * 0.8 * tax)}円</span>
+          </li>
+          <li>
+            30%引き<span>{Math.floor(this.state.price * 0.7 * tax)}円</span>
+          </li>
+          <li>
+            40%引き<span>{Math.floor(this.state.price * 0.6 * tax)}円</span>
+          </li>
+          <li>
+            50%引き<span>{Math.floor(this.state.price * 0.5 * tax)}円</span>
+          </li>
         </ul>
       </>
     );
