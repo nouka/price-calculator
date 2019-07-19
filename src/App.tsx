4fcdfc40d5831308a668107e28fabe3383d40c18
ノーカ <t-nouka@oz-vision.co.jp>
2019-07-19 22:52:24 +0900

    fix: font-size

--- src/App.tsx
+++ src/App.tsx
@@ -20,35 +20,43 @@ class App extends React.Component<Props, State> {
     }
   }
   render() {
-    const tax = 1.08;
     return (
       <>
-        <h1>商品金額計算機</h1>
+        <h1>Price Calculator</h1>
         <input
           type="number"
           pattern="\d*"
           value={this.state.price}
-          placeholder="ここに商品金額(税抜)を入力してください。"
+          placeholder="ここに商品金額(税抜)を入力"
           className="price"
           onChange={e => this.handleChange(e.target.value)}
         />
         <ul>
-          <li>
-            20%引き<span>{Math.floor(this.state.price * 0.8 * tax)}円</span>
-          </li>
-          <li>
-            30%引き<span>{Math.floor(this.state.price * 0.7 * tax)}円</span>
-          </li>
-          <li>
-            40%引き<span>{Math.floor(this.state.price * 0.6 * tax)}円</span>
-          </li>
-          <li>
-            50%引き<span>{Math.floor(this.state.price * 0.5 * tax)}円</span>
-          </li>
+          <Price price={this.state.price} discount={20} />
+          <Price price={this.state.price} discount={30} />
+          <Price price={this.state.price} discount={40} />
+          <Price price={this.state.price} discount={50} />
         </ul>
       </>
     );
   }
 }
+
+interface PriceProps {
+  price: number;
+  discount: number;
+}
+
+const Price = (props: PriceProps) => {
+  const tax = 1.08;
+  return (
+    <li>
+      {props.discount}%Off
+      <span>
+        {Math.floor(props.price * (1 - props.discount / 100) * tax)}円
+      </span>
+    </li>
+  );
+};
 
 export default App;
