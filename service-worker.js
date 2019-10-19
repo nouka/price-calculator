f973012a008252b8e62c56be0840b444d4f3d2d2
ノーカ <t-nouka@oz-vision.co.jp>
2019-10-20 02:02:00 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/price-calculator/precache-manifest.c1183547c7e504e59f94d4f5bef5365e.js"
+  "/price-calculator/precache-manifest.49df09d4fc28f455637937de4893b63b.js"
 );
 
 self.addEventListener('message', (event) => {
